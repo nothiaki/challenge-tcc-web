5d01a567f66226b0e3f0796b05c8e5b41f6fce50
nothiaki <celsohenrique367@gmail.com>
2025-06-29 13:11:17 -0300

    chore(backend): prisma folder

---
+++ backend/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
