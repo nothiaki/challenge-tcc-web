426dbbecc8ce6eb151fe650f8f095e4b6e72b9a6
nothiaki <celsohenrique367@gmail.com>
2025-06-30 20:40:33 -0300

    test(backend): added unit tests

--- backend/src/tasks/tasks.controller.spec.ts
+++ backend/src/tasks/tasks.controller.spec.ts
@@ -1,20 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateDoneTaskDto } from './dto/update-done-tasks.dto';
+import { Task } from './domain/task.domain';
+
+const mockTasksService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  remove: jest.fn(),
+  updateDoneTask: jest.fn(),
+};
 
 describe('TasksController', () => {
-  let controller: TasksController;
+  let tasksController: TasksController;
+  let tasksService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
-      providers: [TasksService],
+      providers: [
+        {
+          provide: TasksService,
+          useValue: mockTasksService
+        }
+      ],
     }).compile();
 
-    controller = module.get<TasksController>(TasksController);
+    tasksController = module.get<TasksController>(TasksController);
+    tasksService = module.get<TasksService>(TasksService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+ describe('create', () => {
+    it('should create a task and return a ResponseTaskDto', async () => {
+      const createTaskDto: CreateTaskDto = {
+        description: 'new task'
+      };
+      const createdTask: Task = {
+        id: '2e8d8cc5-3048-4cd3-b150-3971b26ea686',
+        description: 'task',
+        isActive: true,
+        done: false,
+        createdAt: new Date(),
+      };
+
+      mockTasksService.create.mockResolvedValue(createdTask);
+
+      await tasksController.create(createTaskDto);
+
+      expect(tasksService.create).toHaveBeenCalledWith(createTaskDto);
+    });
   });
+
+  describe('findAll', () => {
+    it('should return an array of ResponseTaskDto', async () => {
+      const tasks: Task[] = [
+        {
+          id: 'a9b61795-74ff-45bf-af24-cc21044adb93',
+          description: 'task 1',
+          isActive: true,
+          done: false,
+          createdAt: new Date(),
+        },
+        {
+          id: 'c56540d4-223c-4411-8f46-3a077f395ab3',
+          description: 'task 2',
+          isActive: true,
+          done: true,
+          createdAt: new Date(),
+        },
+      ];
+
+      mockTasksService.findAll.mockResolvedValue(tasks);
+
+      await tasksController.findAll();
+
+      expect(tasksService.findAll).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('remove', () => {
+    it('should call remove with the given id', async () => {
+      const id = 'b2dac6d9-b3cf-49e0-a8c7-5c53e87b24d9';
+      const removedTask: Task = {
+        id: id,
+        description: 'task',
+        isActive: false,
+        done: false,
+        createdAt: new Date(),
+      };
+
+      mockTasksService.remove.mockResolvedValue(removedTask);
+
+      await tasksController.remove(id);
+
+      expect(tasksService.remove).toHaveBeenCalledWith(id);
+    });
+
+  });
+
+  describe('updateDoneTask', () => {
+    it('should update the done status of a task and return a ResponseTaskDto', async () => {
+      const id = '2f8958ba-9a2b-45da-9d8a-6ade94ec32a0';
+      const updateDoneTaskDto: UpdateDoneTaskDto = { done: true };
+      const updatedTask: Task = {
+        id: id,
+        description: 'task',
+        isActive: true,
+        done: true,
+        createdAt: new Date(),
+      };
+
+      mockTasksService.updateDoneTask.mockResolvedValue(updatedTask);
+
+      await tasksController.updateDoneTask(id, updateDoneTaskDto);
+
+      expect(tasksService.updateDoneTask).toHaveBeenCalledWith(id, updateDoneTaskDto);
+    });
+  });
+
 });
--- backend/src/tasks/tasks.service.spec.ts
+++ backend/src/tasks/tasks.service.spec.ts
@@ -1,18 +1,148 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateDoneTaskDto } from './dto/update-done-tasks.dto';
+
+const mockPrismaService = {
+  task: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    update: jest.fn(),
+  }
+};
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let tasksService: TasksService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService
+        }
+      ],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a task with success', async () => {
+      const createTaskDto: CreateTaskDto = { description: 'new task' };
+      const createdTask = {
+        id: 'd4ec1109-b6c0-416c-a561-57b24d80f4ba',
+        description: 'new task',
+        isActive: true,
+        done: false,
+        createdAt: new Date()
+      };
+
+      mockPrismaService.task.create.mockResolvedValue(createdTask);
+
+      const result = await tasksService.create(createTaskDto);
+
+      expect(mockPrismaService.task.create).toHaveBeenCalledWith({
+        data: {
+          description: createTaskDto.description,
+        },
+      });
+
+      expect(result).toEqual(createdTask);
+    });
   });
+
+  describe('find tasks', () => {
+    it('should find all tasks with succes', async () => {
+      const activeTasks = [
+        {
+          id: '9c281978-fcef-49ef-8fb1-118fef2861de',
+          description: 'task 1',
+          isActive: true,
+          done: false,
+          createdAt: new Date()
+        },
+        {
+          id: '03f491bb-11ce-4c77-8fcc-e00091760d35',
+          description: 'task 2',
+          isActive: true,
+          done: false,
+          createdAt: new Date()
+        }
+      ];
+
+      mockPrismaService.task.findMany.mockResolvedValue(activeTasks);
+
+      const result = await tasksService.findAll();
+
+      expect(mockPrismaService.task.findMany).toHaveBeenCalledWith({
+        where: {
+          isActive: true,
+        },
+      });
+
+      expect(result).toEqual(activeTasks);
+
+    });
+  });
+
+  describe('remove', () => {
+    it('should set isActive to false for a given task', async () => {
+      const id = '23438dcd-daaf-4dd0-8171-bd447f52b233';
+      const updatedTask = {
+        id: id,
+        description: 'task to remove',
+        isActive: false,
+        done: false,
+        createdAt: new Date()
+      };
+
+      mockPrismaService.task.update.mockResolvedValue(updatedTask);
+
+      const result = await tasksService.remove(id);
+
+      expect(mockPrismaService.task.update).toHaveBeenCalledWith({
+        where: { id: id },
+        data: { isActive: false },
+      });
+
+      expect(result).toEqual(updatedTask);
+    });
+  });
+
+  describe('update done task', () => {
+    it('should update the done status of a task', async () => {
+      const id = 'aaa5dec4-5d2a-4888-8125-95cd04cfadc5';
+      const updateDoneTaskDto: UpdateDoneTaskDto = {
+        done: true
+      };
+      const updatedTask = {
+        id: id,
+        description: 'task to update',
+        isActive: true,
+        done: true,
+        createdAt: new Date()
+      };
+
+      mockPrismaService.task.update.mockResolvedValue(updatedTask);
+
+      const result = await tasksService.updateDoneTask(id, updateDoneTaskDto);
+
+      expect(mockPrismaService.task.update).toHaveBeenCalledWith({
+        where: {
+          id: id
+        },
+        data: {
+          done: updateDoneTaskDto.done
+        },
+      });
+
+      expect(result).toEqual(updatedTask);
+    });
+  });
+
 });
